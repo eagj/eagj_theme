60430219aab6a4efd07665d95d78156f55219685
raspi <quique.garcia@gmail.com>
2020-01-04 22:35:36 +0100

    the_loop en index.php

--- index.php
+++ index.php
@@ -3,66 +3,28 @@
     <div class="container-fluid">
         <!--entradas-->
         <div class="row">
-            <!--entrada-->
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+            <!--entrada--> 
             <div class="card-body col-lg-6 col-xl-4 bg-white entrada">
                 <div class="image">
-                    <a href="single.html">
+                    <a href="<?php the_permalink(); ?>">
                         <img src="https://live.staticflickr.com/2896/14066335439_f1cda56a70_c_d.jpg" alt="foto"
                             class="img-fluid">
                     </a>
                 </div>
                 <div class="row">
                     <div class="col-lg-10 mt-n3 mx-auto bg-white">
-                        <p class="small"><a href="#">Categoría</a></p>
+                        <p class="small"><?php the_category();?></p>
 
                         <h2>
-                            <a href="single.html">Título de la entrada</a>
+                            <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
                         </h2>
-                        <p class="small"><a href="#">etiqueta1</a> | <a href="#">etiqueta2</a></p>
+                        <p class="small"><?php the_tags('', '|', '')?></p>
                     </div>
                 </div>
             </div>
             <!--entrada-->
-            <!--entrada-->
-            <div class="card-body col-lg-6 col-xl-4 bg-white entrada">
-                <div class="image">
-                    <a href="single.html">
-                        <img src="https://live.staticflickr.com/2911/14211943025_d42cb25f67_c_d.jpg" alt="foto"
-                            class="img-fluid">
-                    </a>
-                </div>
-                <div class="row">
-                    <div class="col-lg-10 mt-n3 mx-auto bg-white">
-                        <p class="small"><a href="#">Categoría</a></p>
-
-                        <h2>
-                            <a href="single.html">Título de la entrada</a>
-                        </h2>
-                        <p class="small"><a href="#">etiqueta1</a> | <a href="#">etiqueta2</a></p>
-                    </div>
-                </div>
-            </div>
-            <!--entrada-->
-            <!--entrada-->
-            <div class="card-body col-lg-6 col-xl-4 bg-white entrada">
-                <div class="image">
-                    <a href="single.html">
-                        <img src="https://live.staticflickr.com/7310/13954588782_19b37c25e6_c_d.jpg" alt="foto"
-                            class="img-fluid">
-                    </a>
-                </div>
-                <div class="row">
-                    <div class="col-lg-10 mt-n3 mx-auto bg-white">
-                        <p class="small"><a href="#">Categoría</a></p>
-
-                        <h2>
-                            <a href="single.html">Título de la entrada</a>
-                        </h2>
-                        <p class="small"><a href="#">etiqueta1</a> | <a href="#">etiqueta2</a></p>
-                    </div>
-                </div>
-            </div>
-            <!--entrada-->
+            <?php endwhile; endif; ?>            
         </div>
         <!--entradas-->
 
@@ -80,5 +42,5 @@
         </div>
         <!--Paginacion-->
     </div>
-    
+
   <?php get_footer();?>
